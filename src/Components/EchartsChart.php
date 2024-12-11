a10ff317b95f2edc58302fe87e8a520a72210352
Harrison <35530362+Nano112@users.noreply.github.com>
2024-12-11 11:30:24 +0000

    update echarts library to version 5.5.1 and refactor chart initialization logic

--- src/Components/EchartsChart.php
+++ src/Components/EchartsChart.php
@@ -15,55 +15,107 @@ class EchartsChart extends Component
 
     public static function getDefaultOptions($dataPoints = [], $chartType = 'line')
     {
+        // Common style constants
+        $axisLineColor = '#E0E6F1';
+        $axisFontColor = '#6E7079';
+        $tooltipBgColor = '#FFFFFF';
+        $tooltipBorderColor = '#E0E6F1';
+        $gridLineColor = '#E0E6F1';
+        $emphasisColor = '#5470C6';
+
         $tooltipFormatter = <<<'JS'
-            function(params) {
-                if (Array.isArray(params)) {
-                    return params[0].name + '<br/>' + 
-                        params.map(param => 
-                            `<span style="display:inline-block;margin-right:5px;border-radius:50%;width:10px;height:10px;background-color:${param.color};"></span>` +
-                            `${param.seriesName}: ${param.value}`
-                        ).join('<br/>');
-                }
-                return `${params.seriesName}<br/>${params.name}: ${params.value}`;
+        function(params) {
+            if (Array.isArray(params)) {
+                return params[0].name + '<br/>' + 
+                    params.map(param => 
+                        `<span style="display:inline-block;margin-right:5px;border-radius:50%;width:10px;height:10px;background-color:${param.color};"></span>` +
+                        `${param.seriesName}: ${param.value}`
+                    ).join('<br/>');
             }
-        JS;
-    
+            return `${params.seriesName}<br/>${params.name}: ${params.value}`;
+        }
+    JS;
+
         $pieTooltipFormatter = <<<'JS'
-            function(params) {
-                return `${params.seriesName}<br/>${params.name}: ${params.value} (${params.percent}%)`;
-            }
-        JS;
-    
+        function(params) {
+            return `${params.seriesName}<br/>${params.name}: ${params.value} (${params.percent}%)`;
+        }
+    JS;
+
         $baseOptions = [
             'tooltip' => [
-                'show' => true,
-                'trigger' => $chartType === 'pie' ? 'item' : 'item',
-                'formatter' => $chartType === 'pie' ? $pieTooltipFormatter : $tooltipFormatter,
-                'backgroundColor' => 'rgba(255, 255, 255, 0.9)',
-                'borderColor' => '#ccc',
+                'trigger' => 'axis',
+                'backgroundColor' => $tooltipBgColor,
                 'borderWidth' => 1,
+                'borderColor' => $tooltipBorderColor,
+                'padding' => [8, 12],
                 'textStyle' => [
-                    'color' => '#333'
+                    'color' => '#333',
+                    'fontSize' => 12
                 ],
                 'axisPointer' => [
-                    'type' => 'cross',
-                    'label' => [
-                        'backgroundColor' => '#6a7985'
-                    ],
-                    'snap' => true
+                    'type' => 'shadow',
+                    'shadowStyle' => [
+                        'color' => 'rgba(0,0,0,0.05)'
+                    ]
                 ]
             ],
             'grid' => [
                 'left' => '3%',
                 'right' => '4%',
-                'bottom' => '3%',
+                'bottom' => '15%', // Increased to accommodate data zoom
+                'top' => '15%',    // Increased to accommodate legend
                 'containLabel' => true
             ],
             'toolbox' => [
+                'show' => true,
                 'feature' => [
-                    'saveAsImage' => ['title' => 'Save'],
-                    'dataZoom' => ['title' => ['zoom' => 'Zoom', 'back' => 'Reset Zoom']],
-                    'restore' => ['title' => 'Reset']
+                    'saveAsImage' => [
+                        'title' => 'Save',
+                        'icon' => 'path://M19,12v7L5,19v-7h14m0-5H5c-1.1,0-2,0.9-2,2v10c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V9C21,7.9,20.1,7,19,7z M17,16H7v-2h10V16z M17,12H7v-2h10V12z'
+                    ],
+                    'dataZoom' => [
+                        'yAxisIndex' => 'none',
+                        'title' => [
+                            'zoom' => 'Zoom',
+                            'back' => 'Reset Zoom'
+                        ]
+                    ],
+                    'restore' => [
+                        'title' => 'Reset'
+                    ]
+                ],
+                'itemSize' => 15,
+                'emphasis' => [
+                    'iconStyle' => [
+                        'borderColor' => $emphasisColor
+                    ]
+                ]
+            ],
+            'dataZoom' => [
+                [
+                    'type' => 'inside',
+                    'start' => 0,
+                    'end' => 100,
+                    'minValueSpan' => 5
+                ],
+                [
+                    'show' => true,
+                    'type' => 'slider',
+                    'start' => 0,
+                    'end' => 100,
+                    'height' => 20,
+                    'bottom' => 0,
+                    'borderColor' => 'transparent',
+                    'backgroundColor' => '#f7f7f7',
+                    'fillerColor' => 'rgba(84,112,198,0.2)',
+                    'handleStyle' => [
+                        'color' => '#fff',
+                        'shadowBlur' => 3,
+                        'shadowColor' => 'rgba(0, 0, 0, 0.6)',
+                        'shadowOffsetX' => 2,
+                        'shadowOffsetY' => 2
+                    ]
                 ]
             ],
             'legend' => [
@@ -71,103 +123,213 @@ class EchartsChart extends Component
                 'type' => 'scroll',
                 'orient' => $chartType === 'pie' ? 'vertical' : 'horizontal',
                 'left' => $chartType === 'pie' ? 10 : 'center',
-                'top' => $chartType === 'pie' ? 'middle' : 10
+                'top' => 5,
+                'textStyle' => [
+                    'color' => $axisFontColor
+                ],
+                'pageTextStyle' => [
+                    'color' => $axisFontColor
+                ],
+                'pageIconColor' => $axisFontColor,
+                'pageIconInactiveColor' => '#aaa'
             ]
         ];
-    
+
         $typeSpecificOptions = [
             'line' => [
                 'xAxis' => [
                     'type' => 'category',
                     'boundaryGap' => false,
-                    'axisLine' => ['show' => true],
-                    'axisLabel' => ['show' => true],
-                    'splitLine' => ['show' => true, 'lineStyle' => ['type' => 'dashed']]
+                    'axisLine' => [
+                        'show' => true,
+                        'lineStyle' => ['color' => $axisLineColor]
+                    ],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'show' => true,
+                        'color' => $axisFontColor
+                    ],
+                    'splitLine' => [
+                        'show' => true,
+                        'lineStyle' => [
+                            'color' => $gridLineColor,
+                            'type' => 'dashed'
+                        ]
+                    ]
                 ],
                 'yAxis' => [
                     'type' => 'value',
-                    'splitLine' => ['show' => true, 'lineStyle' => ['type' => 'dashed']]
+                    'axisLine' => ['show' => false],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'color' => $axisFontColor
+                    ],
+                    'splitLine' => [
+                        'show' => true,
+                        'lineStyle' => [
+                            'color' => $gridLineColor,
+                            'type' => 'dashed'
+                        ]
+                    ]
                 ],
                 'series' => [[
                     'type' => 'line',
                     'smooth' => true,
-                    'symbol' => 'circle',
-                    'symbolSize' => 6,
+                    'symbol' => 'emptyCircle',
+                    'symbolSize' => 8,
                     'showSymbol' => true,
+                    'lineStyle' => [
+                        'width' => 2
+                    ],
                     'emphasis' => [
                         'focus' => 'series',
                         'itemStyle' => [
                             'borderWidth' => 2
                         ]
-                    ]
+                    ],
+                    'animation' => true,
+                    'animationDuration' => 1000,
+                    'animationEasing' => 'cubicOut'
                 ]]
             ],
             'bar' => [
                 'xAxis' => [
                     'type' => 'category',
-                    'axisLabel' => ['show' => true],
-                    'axisLine' => ['show' => true],
-                    'splitLine' => ['show' => false]
+                    'axisLine' => [
+                        'show' => true,
+                        'lineStyle' => ['color' => $axisLineColor]
+                    ],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'show' => true,
+                        'color' => $axisFontColor
+                    ]
                 ],
                 'yAxis' => [
                     'type' => 'value',
-                    'splitLine' => ['show' => true, 'lineStyle' => ['type' => 'dashed']]
+                    'axisLine' => ['show' => false],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'color' => $axisFontColor
+                    ],
+                    'splitLine' => [
+                        'show' => true,
+                        'lineStyle' => [
+                            'color' => $gridLineColor,
+                            'type' => 'dashed'
+                        ]
+                    ]
                 ],
                 'series' => [[
                     'type' => 'bar',
                     'barMaxWidth' => 50,
+                    'itemStyle' => [
+                        'borderRadius' => [4, 4, 0, 0]
+                    ],
                     'emphasis' => [
-                        'focus' => 'series'
-                    ]
+                        'focus' => 'series',
+                        'itemStyle' => [
+                            'borderWidth' => 2
+                        ]
+                    ],
+                    'animation' => true,
+                    'animationDuration' => 1500,
+                    'animationEasing' => 'elasticOut'
                 ]]
             ],
             'pie' => [
                 'series' => [[
                     'type' => 'pie',
                     'radius' => ['50%', '70%'],
+                    'center' => ['50%', '50%'],
                     'avoidLabelOverlap' => true,
+                    'itemStyle' => [
+                        'borderRadius' => 4,
+                        'borderColor' => '#fff',
+                        'borderWidth' => 2
+                    ],
                     'label' => [
-                        'show' => false,
-                        'position' => 'center'
+                        'show' => false
                     ],
                     'emphasis' => [
                         'label' => [
                             'show' => true,
                             'fontSize' => 14,
                             'fontWeight' => 'bold'
+                        ],
+                        'itemStyle' => [
+                            'shadowBlur' => 10,
+                            'shadowOffsetX' => 0,
+                            'shadowColor' => 'rgba(0, 0, 0, 0.5)'
                         ]
                     ],
                     'labelLine' => [
                         'show' => false
-                    ]
+                    ],
+                    'animation' => true,
+                    'animationDuration' => 1000,
+                    'animationEasing' => 'circularOut'
                 ]]
             ],
             'scatter' => [
                 'xAxis' => [
                     'type' => 'value',
-                    'splitLine' => ['show' => true, 'lineStyle' => ['type' => 'dashed']]
+                    'scale' => true,
+                    'axisLine' => [
+                        'show' => true,
+                        'lineStyle' => ['color' => $axisLineColor]
+                    ],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'color' => $axisFontColor
+                    ],
+                    'splitLine' => [
+                        'show' => true,
+                        'lineStyle' => [
+                            'color' => $gridLineColor,
+                            'type' => 'dashed'
+                        ]
+                    ]
                 ],
                 'yAxis' => [
                     'type' => 'value',
-                    'splitLine' => ['show' => true, 'lineStyle' => ['type' => 'dashed']]
+                    'scale' => true,
+                    'axisLine' => ['show' => false],
+                    'axisTick' => ['show' => false],
+                    'axisLabel' => [
+                        'color' => $axisFontColor
+                    ],
+                    'splitLine' => [
+                        'show' => true,
+                        'lineStyle' => [
+                            'color' => $gridLineColor,
+                            'type' => 'dashed'
+                        ]
+                    ]
                 ],
                 'series' => [[
                     'type' => 'scatter',
-                    'symbolSize' => 10,
+                    'symbolSize' => 12,
+                    'symbol' => 'circle',
+                    'itemStyle' => [
+                        'opacity' => 0.8
+                    ],
                     'emphasis' => [
                         'focus' => 'series',
                         'itemStyle' => [
-                            'borderWidth' => 2
+                            'borderWidth' => 2,
+                            'shadowBlur' => 10,
+                            'shadowColor' => 'rgba(0,0,0,0.3)'
                         ]
                     ]
                 ]]
             ]
         ];
-    
+
         if (!isset($typeSpecificOptions[$chartType])) {
             return array_merge($baseOptions, $typeSpecificOptions['line']);
         }
-    
+
         return array_merge($baseOptions, $typeSpecificOptions[$chartType]);
     }
 
@@ -178,9 +340,9 @@ class EchartsChart extends Component
         $this->theme = $theme;
         $this->events = $events;
         $this->callbacks = $callbacks;
-    
+
         $defaultOptions = self::getDefaultOptions($options['legend']['data'] ?? [], $chartType);
-        
+
         // Don't modify series configuration
         $this->options = array_replace_recursive($defaultOptions, $options);
     }
--- src/resources/views/components/echarts-chart.blade.php
+++ src/resources/views/components/echarts-chart.blade.php
@@ -1,126 +1,104 @@
 <div class="bg-white h-full w-full">
     @assets
         @once
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/echarts/5.4.3/echarts.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js"></script>
         @endonce
     @endassets
 
-    <div wire:ignore x-data="echartsComponent_{!! $chartId !!}" x-init="init" x-on:beforeunmount.window="cleanup" class="w-full h-full">
-        <div x-ref="chart" class="w-full h-full"></div>
-    </div>
+    <div wire:ignore class="w-full h-full" id="{{ $chartId }}"></div>
 
     @script
         <script data-navigate-once>
-            Alpine.data('echartsComponent_{!! $chartId !!}', () => ({
-                chart: null,
-                eventHandlers: {},
-                resizeObserver: null,
-
-                init() {
-                    const options = @json($options);
-                    
-                    // Handle tooltip formatters by converting strings to functions
-                    if (options.tooltip?.formatter && typeof options.tooltip.formatter === 'string') {
-                        try {
-                            // Get just the function body between the curly braces
-                            const matches = options.tooltip.formatter.match(/\{([\s\S]*)\}/);
-                            if (matches && matches[1]) {
-                                const functionBody = matches[1].trim();
-                                options.tooltip.formatter = new Function('params', functionBody);
-                            }
-                        } catch (e) {
-                            console.error('Error parsing tooltip formatter:', e);
-                        }
-                    }
+        (function() {
+            let chart = null;
+            let resizeObserver = null;
+            const chartElement = document.getElementById('{{ $chartId }}');
             
-                    if (!this.$refs.chart) {
-                        console.warn('Chart element not found, skipping initialization');
-                        return;
-                    }
-            
-                    this.chart = echarts.init(this.$refs.chart, @json($theme));
-                    this.chart.setOption(options);
-                    
-                    this.setupEventHandlers();
-                    this.setupResizeObserver();
-                },
-
-                setupEventHandlers() {
-                    const chartId = @json($chartId);
-                    const events = @json($events);
-                    
-                    // Setup default updateChartOptions handler
-                    Livewire.on('updateChartOptions', (data) => {
-                        if (!data.chartId) {
-                            console.error('Chart ' + @json($chartId) +
-                                ': No chartId provided in the updateChartOptions event');
-                            console.log(data);
-                            return;
-                        }
-                        if (data.chartId !== @json($chartId)) {
-                            return;
-                        }
-                        if (this.chart && data) {
-                            this.chart.setOption(data.options, data.notMerge, data.lazyUpdate);
-                            
-                        }
-                    });
-
-                    // Setup custom events
-                    const context = { chart: this.chart, chartId };
-                    
-                    for (const [event, handler] of Object.entries(events)) {
-                        try {
-                            const wrappedHandler = new Function(
-                                ...Object.keys(context),
-                                `return ${handler}`
-                            );
-                            
-                            this.eventHandlers[event] = ((data) => {
-                                if (!data.chartId || data.chartId !== chartId) return;
-                                wrappedHandler(...Object.values(context))(data);
-                            }).bind(this);
-
-                            Livewire.on(event, this.eventHandlers[event]);
-                        } catch (e) {
-                            console.error(`Error binding event ${event}:`, e);
-                        }
-                    }
-                },
-
-                setupResizeObserver() {
-                    if (this.resizeObserver) {
-                        this.resizeObserver.disconnect();
-                        this.resizeObserver = null;
-                    }
-
-                    this.resizeObserver = new ResizeObserver(() => {
-                        if (this.chart) {
-                            this.chart.resize();
-                        }
-                    });
-
-                    if (this.$refs.chart) {
-                        this.resizeObserver.observe(this.$refs.chart);
-                    }
-                },
-
-                cleanup() {
-                    if (this.resizeObserver) {
-                        this.resizeObserver.disconnect();
-                        this.resizeObserver = null;
-                    }
-                    if (this.chart) {
-                        this.chart.dispose();
-                        this.chart = null;
-                    }
-                    // Clean up event handlers
-                    for (const [event, handler] of Object.entries(this.eventHandlers)) {
-                        Livewire.off(event, handler);
-                    }
-                    this.eventHandlers = {};
+            function parseOptions(options) {
+                // Deep clone the options to avoid reference issues
+                const parsedOptions = JSON.parse(JSON.stringify(options));
+                
+                // Convert formatter string to function if it exists
+                if (parsedOptions.tooltip?.formatter && 
+                    typeof parsedOptions.tooltip.formatter === 'string' && 
+                    parsedOptions.tooltip.formatter.includes('function')) {
+                    parsedOptions.tooltip.formatter = eval('(' + parsedOptions.tooltip.formatter + ')');
                 }
-            }));
+                
+                return parsedOptions;
+            }
+
+            function initChart() {
+                if (!chartElement) {
+                    console.warn('Chart element not found');
+                    return;
+                }
+
+                const initialOptions = parseOptions(@json($options));
+
+                chart = echarts.init(chartElement, @json($theme));
+                chart.setOption(initialOptions);
+                setupResizeObserver();
+                setupEventHandlers();
+            }
+
+            function setupEventHandlers() {
+                Livewire.on('updateChartOptions', (data) => {
+                    if (!data.chartId || data.chartId !== '{{ $chartId }}') return;
+                    if (chart) {
+                        const updatedOptions = parseOptions(data.options);
+                        chart.setOption(updatedOptions, true);
+                    }
+                });
+            }
+
+            function setupResizeObserver() {
+                if (resizeObserver) {
+                    resizeObserver.disconnect();
+                }
+
+                resizeObserver = new ResizeObserver(entries => {
+                    if (chart) {
+                        chart.resize();
+                    }
+                });
+
+                if (chartElement) {
+                    resizeObserver.observe(chartElement);
+                }
+
+                // Also listen for window resize as a fallback
+                window.addEventListener('resize', () => {
+                    if (chart) {
+                        chart.resize();
+                    }
+                });
+            }
+
+            function cleanup() {
+                if (resizeObserver) {
+                    resizeObserver.disconnect();
+                    resizeObserver = null;
+                }
+                if (chart) {
+                    chart.dispose();
+                    chart = null;
+                }
+                // Remove window resize listener
+                window.removeEventListener('resize', () => {
+                    if (chart) {
+                        chart.resize();
+                    }
+                });
+            }
+
+            // Initialize the chart
+            initChart();
+
+            // Cleanup on page navigation or component removal
+            window.addEventListener('beforeunload', cleanup);
+            document.addEventListener('livewire:navigating', cleanup);
+        })();
         </script>
     @endscript
 </div>
